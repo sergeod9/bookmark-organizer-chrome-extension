65b372bb0a522381b274319bba3b7d944cb2846e
Georges Dahdouh <georges.dahdouh@gmail.com>
2022-08-16 02:30:47 +0300

    Added content view
    Added content view and implemented double click folder to switch to content view, home button to reload page

---
+++ htmlVariables.js
@@ -0,0 +1,20 @@
+const foldersView = `
+                    <header id="folders-menu">
+                         <button id="add-folder-btn">+ New Folder</button>
+                        <input type="text" placeholder="Enter folder name" id="folder-name-input" />
+                    </header>
+                    <section id="folders-container">
+
+                    </section>
+                    `
+
+const contentView = `
+                    <header id="content-menu">
+                        <button id="add-tab-btn">Save Tab</button>
+                        <button id="home-btn">Home</button>
+                    </header>
+                    <section id="content-container">
+                        
+                    </section>
+
+                    `
--- index.js
+++ index.js
@@ -1,62 +1,102 @@
-const addFolderBtn = document.getElementById('add-folder-btn')
-const foldersContainer = document.getElementById('folders-container')
-const newFolderNameInput = document.getElementById('folder-name-input')
-
-let foldersArr = []
-let foldersElements = ""
-let selectedFolder = ""
-
-addFolderBtn.addEventListener('click', addFolder)
-
-renderFolders()
+const root = document.getElementById('root')
 
 
-function addFolder(){
-    if(newFolderNameInput.value){
-        foldersArr.push({name: newFolderNameInput.value, content:[""]})
-        newFolderNameInput.value = ""
-        localStorage.setItem("Folders",JSON.stringify(foldersArr))
-    }
+
+let view = "folders-view"
+
+
+if (view === 'folders-view'){
+    root.innerHTML = foldersView
+
+    const addFolderBtn = document.getElementById('add-folder-btn')
+    const foldersContainer = document.getElementById('folders-container')
+    const newFolderNameInput = document.getElementById('folder-name-input')
+
+    let foldersArr = []
+    let foldersHTML = ""
+    let selectedFolder = ""
+
+    addFolderBtn.addEventListener('click', addFolder)
+
     renderFolders()
-}
+    foldersElements = document.querySelectorAll('.folder')
+    for (let fe of foldersElements){
+        fe.addEventListener('dblclick', (e)=> openFolder(e.currentTarget) )// currentTarget captures the event owner, even when a child is clicked
+    }
 
-function renderFolders(){
-    foldersContainer.innerHTML =""
-    foldersArr = JSON.parse(localStorage.getItem('Folders'))
-    if(foldersArr){
-        for (let folder of foldersArr){
-            foldersElements += createFolderWidget(folder.name)//check scripts in index.html
+        
+    function addFolder(){
+        if(newFolderNameInput.value){
+            foldersArr.push({name: newFolderNameInput.value, content:[""]})
+            newFolderNameInput.value = ""
+            localStorage.setItem("Folders",JSON.stringify(foldersArr))
         }
-        foldersContainer.innerHTML = foldersElements // append created folder to folders container
-        foldersElements =""
+        renderFolders()
     }
-    else{
-        foldersArr = []
-        foldersContainer.innerHTML = `<p class="info-message">Add some folders</p>`
+    
+    function renderFolders(){
+        foldersContainer.innerHTML =""
+        foldersArr = JSON.parse(localStorage.getItem('Folders'))
+        if(foldersArr){
+            console.log('folders retrieved')
+            for (let folder of foldersArr){
+                foldersHTML += createFolderWidget(folder.name)//check scripts in index.html
+            }
+            foldersContainer.innerHTML = foldersHTML // append created folder to folders container
+            foldersHTML =""
+        }
+        else{
+            foldersArr = []
+            foldersContainer.innerHTML = `<p class="info-message">Add some folders</p>`
+        }
     }
-}
 
-function selectFolder(folder){
-    if (selectedFolder === folder){
-        selectedFolder = ""
-    }
-    else{
-        selectedFolder = folder
-        const allFolders = document.querySelectorAll(".folder")
-        for (f of allFolders){
-            if (f !== folder){
-                f.classList.remove("selected-folder")
+    
+    function selectFolder(folder){
+        if (selectedFolder === folder){
+            selectedFolder = ""
+        }
+        else{
+            selectedFolder = folder
+            const allFolders = document.querySelectorAll(".folder")
+            for (f of allFolders){
+                if (f !== folder){
+                    f.classList.remove("selected-folder")
+                }
             }
         }
+        manageFolderSelection(folder)
     }
-    manageFolderSelection(folder)
+    
+    function manageFolderSelection(folder){
+        if (selectedFolder){
+            folder.classList.add('selected-folder')
+        }
+        else{
+            folder.classList.remove('selected-folder')
+        }
+    }
+    
 }
 
-function manageFolderSelection(folder){
-    if (selectedFolder){
-        folder.classList.add('selected-folder')
-    }
-    else{
-        folder.classList.remove('selected-folder')
+function openFolder(e){
+    root.innerHTML = contentView
+    view = "content-view"
+    
+    const homeBtn = document.getElementById('home-btn')
+    console.log(homeBtn)
+    homeBtn.addEventListener('click', backToHome )
+
+    function backToHome(){
+        view = 'folders-view'
+        location.reload()
+        // chrome.runtime.reload()
     }
 }
+
+function renderContent(){
+
+}
+
+
+
